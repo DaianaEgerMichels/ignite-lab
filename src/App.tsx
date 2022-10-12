f11e4e27bab535a74b82d032e30ca53a1159338f
Daiana Michels <daianaem.12@gmail.com>
2022-10-11 22:20:05 -0300

    add new configs

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import './styles/global.css';
 export function App() {
   
   return (
-    <h1>Hello world!</h1>
+    <>
+    <h1 className='font-bold text-2xl text-gray-800'>Hello world!</h1>
+    <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300">Enviar</button>
+    </>
   )
 }
 
